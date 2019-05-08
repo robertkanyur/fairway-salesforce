2d43889f9752810039674301abafaca72feabeef
Robert Kanyur <robert@kanyur.com>
2019-05-08 14:54:10 -0700

    Updated email test script

--- mailgun.js
+++ mailgun.js
@@ -2,22 +2,30 @@ require('dotenv').config()
 const path = require('path');
 const fs = require('fs');
 
-const emailFilename = process.argv[2];
 const MAILGUN_API = process.env.MAILGUN_API;
 const domain = 'mg.fairway321.com';
 
-templatePath = path.join(__dirname, 'dist', emailFilename + '.html');
-const emailContent = fs.readFileSync(templatePath).toString();
+for (i = 2; i < (process.argv.length); i++) {
 
-const mailgun = require('mailgun-js')({apiKey: MAILGUN_API, domain: domain});
+  let emailFilename = process.argv[i];
 
-let data = {
-  from: 'Robert Kanyur <robert@fairway321.com>',
-  to: '9rx33xgd@gmail.com',
-  subject: emailFilename + ' ' + Date.now(),
-  html: emailContent
-}
+  templatePath = path.join(__dirname, 'dist', emailFilename + '.html');
+  const emailContent = fs.readFileSync(templatePath).toString();
 
-mailgun.messages().send(data, function(error, body){
-  console.log(body);
-});
+  const mailgun = require('mailgun-js')({apiKey: MAILGUN_API, domain: domain});
+
+  function generateData(){
+    return({
+      from: 'Robert Kanyur <robert@fairway321.com>',
+      to: '9rx33xgd@gmail.com',
+      subject: emailFilename + ' ' + Date.now(),
+      html: emailContent
+    })
+  };
+
+  let data = generateData();
+
+  mailgun.messages().send(data, function(error, body){
+    console.log(body);
+  })
+};
